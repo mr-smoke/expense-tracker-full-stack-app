4b49d597c6afdbe5472d6345f24c569ba850b887
baki duman <g.s_baki_70@hotmail.com>
2024-09-20 13:04:10 +0300

    User resolver updated to include authentication and session management.

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,12 +1,115 @@
+import User from "../models/user.model.js";
 import { users } from "../data/data.js";
+import bcrypt from "bcryptjs";
 
 const useResolver = {
   Query: {
     getUsers: async () => {
       return users;
     },
+    authUser: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        if (!user) {
+          throw new Error("Unauthorized");
+        }
+        return user;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message || "An error occurred");
+      }
+    },
+    getUser: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new Error("User not found");
+        }
+        return user;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message || "An error occurred");
+      }
+    },
   },
-  Mutation: {},
+  Mutation: {
+    register: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+          throw new Error("Username already exists");
+        }
+
+        const user = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+        });
+
+        await user.save();
+        await context.login(user);
+        return newUser;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message || "An error occurred");
+      }
+    },
+    login: async (_, { input }, context) => {
+      try {
+        const { username, password } = input;
+
+        if (!username || !password) {
+          throw new Error("All fields are required");
+        }
+
+        // const user = await User.findOne({ username });
+        // if (!user) {
+        //   throw new Error("Invalid credentials");
+        // }
+
+        // const isPasswordValid = await bcrypt.compare(password, user.password);
+        // if (!isPasswordValid) {
+        //   throw new Error("Invalid credentials");
+        // }
+
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          password,
+        });
+
+        await context.login(user);
+        return user;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message || "An error occurred");
+      }
+    },
+    logout: async (_, __, context) => {
+      try {
+        await context.logout();
+        context.req.session.destroy((error) => {
+          if (error) {
+            throw new Error("An error occurred");
+          }
+        });
+        context.res.clearCookie("connect.sid");
+
+        return { message: "Logged out" };
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message || "An error occurred");
+      }
+    },
+  },
 };
 
 export default useResolver;
