e474f3693126df1041c1d4603539458181f743dc
baki duman <g.s_baki_70@hotmail.com>
2024-09-20 15:06:41 +0300

    Add routes and made acernity configuration.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GridBackgroundDemo } from "./components/ui/Background";
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <GridBackgroundDemo>
+          <p>some</p>
+          <main>
+            <Routes>
+              <Route path="/" component={<Home />} exact />
+            </Routes>
+          </main>
+        </GridBackgroundDemo>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
