f9681a32139236519d7cd38c76958ed75d520389
baki duman <g.s_baki_70@hotmail.com>
2024-09-20 11:00:26 +0300

    Add transaction and user models.

---
+++ backend/models/transaction.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["cash", "card"],
+  },
+  category: {
+    type: String,
+    enum: ["income", "expense", "saving"],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    default: "Unknown",
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
