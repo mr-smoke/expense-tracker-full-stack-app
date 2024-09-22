76a22638d86e7594405ec42974bc11291de979ae
baki duman <g.s_baki_70@hotmail.com>
2024-09-22 10:33:46 +0300

    Add Apollo Client configuration to connect to the GraphQ.

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ const server = new ApolloServer({
 await server.start();
 
 app.use(
-  "/",
+  "/graphql",
   cors({
     origin: "http://localhost:3000",
     credentials: true,
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GridBackground } from "./components/ui/Background";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+  credentials: "include",
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GridBackground>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </GridBackground>
   </StrictMode>
 );
